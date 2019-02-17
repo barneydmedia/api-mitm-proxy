cc202a51551ca059b13b0f7de6fa5626abd6406d
Barney Davis <barneydmedia@gmail.com>
2019-02-17 18:11:52 -0500

    add basic json api proxying

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const rp = require('request-promise-native');
+const sqlite = require('sqlite');
+const SQL = require('sql-template-strings');
+let db;
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+async function main() {
+    db = await sqlite.open('./database.sqlite', { Promise });
+    console.log('DB connected');
+    db.run(`
+        CREATE TABLE IF NOT EXISTS url_cache (
+            id INTEGER PRIMARY KEY, 
+            url TEXT,
+            response TEXT,
+            created TEXT,
+            updated TEXT
+        );
+    `);
+    console.log('Migration Done');
+}
+main();
+
+// var indexRouter = require('./routes/index');
 
 var app = express();
 
@@ -14,7 +33,44 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+process.on('unhandledRejection', (e, p) => {
+    console.debug(e,p);
+});
+
+function blacklistHeaders(headers) {
+    delete headers.host;
+    delete headers['postman-token'];
+    delete headers.remoteurl;
+    delete headers['accept-encoding'];
+
+    return headers;
+}
+
+app.use('/', async (req, res) => {
+    const defaultHost = 'business.untappd.com';
+    const remoteUrl = `https://${req.headers.remoteurl || defaultHost}${req.originalUrl}`;
+    const storedCount = (await db.get(SQL`SELECT COUNT(url) as url_count FROM url_cache WHERE url = ${remoteUrl};`)).url_count;
+
+    if (!storedCount) {
+        let error = false;
+        const headers = blacklistHeaders({...req.headers});
+        const remReq = await rp({
+            uri: remoteUrl,
+            method: req.method,
+            cache: 'no-cache',
+            headers,
+        }).catch(err => {
+            debugger;
+            res.status(err.statusCode).json(err)
+            error = true;
+        });
+
+        if (error) return;
+        await db.run(SQL`INSERT INTO url_cache (url, response) VALUES (${remoteUrl}, ${remReq})`);
+    }
+    const storedRes = await db.get(SQL`SELECT response FROM url_cache WHERE url = ${remoteUrl};`);
+
+    res.status(200).json(JSON.parse(storedRes.response));
+});
 
 module.exports = app;
